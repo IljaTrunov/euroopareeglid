b45dc7b8f8d8beab01c4a105272b90076b136cbf
Trunyashkaaaaaa <93324206+IljaTrunov@users.noreply.github.com>
2022-02-05 14:17:23 +0200

    Update module1.py

--- euroopareeglid/module1.py
+++ euroopareeglid/module1.py
@@ -1,36 +1,16 @@
-﻿from gtts import gTTS
-import os
-def sonastik():
-        sonastik={}
-        countries=[]
-        capitals=[]
-        file=open("reeglid.txt","r")
-        for line in file:
-            k, v=line.strip().split("-")
-            sonastik[k.strip()]=v.strip()
-            countries.append(k)
-            capitals.append(sonastik[k.strip()])
-        file.close()
-        print(sonastik)
-        print("Countries: ")
-        print(countries)
-        print("Capitals:")
-        print(capitals)
-        s=gTTS(text=capitals[6],lang="en",slow=True).save("reeglid.mp3")
-        os.system("reeglid.mp3")
-        a=input()
+#from gtts import gTTS
+#import os
 
 
-
-def country ():
-	countries=input("Введите страну - ")
-	capitals=input("Введите столицу - ")
-	with open("reeglid.txt", "a") as countries:
-		countries.write(country+"\n")
-	with open("capitals.txt", "a") as capitals: 
-		capitals.write(capital+"\n")
-def count():
-    for country in Countries:
-        country=input("Enter your country:")
-    if Countries in Capitals:
-        print("The capital of country:" +country +" - "+Capitals[country])
+#def country ():
+	#countries=input("Введите страну - ")
+	#capitals=input("Введите столицу - ")
+	#with open("reeglid.txt", "a") as countries:
+		#countries.write(country+"\n")
+	#with open("capitals.txt", "a") as capitals: 
+		#capitals.write(capital+"\n")
+#def count():
+    #for country in Countries:
+        #country=input("Введи страну:")
+    #if Countries in Capitals:
+        #print("Столица этой страны:" +country +" - "+Capitals[country])
